2e2c653e7e43a379dcbdbf66dea939aa8e9d8975
Leander Herzog <info@leanderherzog.ch>
2013-03-25 01:37:51 +0100

    setup

---
+++ _site/js/main.js
@@ -0,0 +1,56 @@
+  $(function() { 
+    // Setup the player to autoplay the next track
+var a = audiojs.createAll({
+  play: function() {
+  //  console.log(this);
+  },
+  trackEnded: function() {
+    var next = $('ol li.playing').next();
+    if (!next.length) next = $('ol li').first();
+    next.addClass('playing').siblings().removeClass('playing');
+    audio.load($('a', next).attr('data-src'));
+    audio.play();
+  }
+});
+
+// Load in the first track
+var audio = a[0];
+    first = $('ol a').attr('data-src');
+//$('ol li').first().addClass('playing');
+audio.load(first);
+
+// Load in a track on click
+$('ol li').click(function(e) {
+  var trackname = $(this).html();
+  $(".intro h1").html(trackname);
+  var ee= $(this).find("a").data("background");
+  $("body").css("background-image","url("+ee+")");
+  if($(this).hasClass("playing")){
+  audio.pause();
+  $(this).removeClass('playing');
+  }else{
+  e.preventDefault();
+  $(this).addClass('playing').siblings().removeClass('playing');
+  audio.load($('a', this).attr('data-src'));
+  audio.play();
+  }
+});
+// Keyboard shortcuts
+$(document).keydown(function(e) {
+  var unicode = e.charCode ? e.charCode : e.keyCode;
+     // right arrow
+  if (unicode == 39) {
+    var next = $('li.playing').next();
+    if (!next.length) next = $('ol li').first();
+    next.click();
+    // back arrow
+  } else if (unicode == 37) {
+    var prev = $('li.playing').prev();
+    if (!prev.length) prev = $('ol li').last();
+    prev.click();
+    // spacebar
+  } else if (unicode == 32) {
+    audio.playPause();
+  }
+})
+});
