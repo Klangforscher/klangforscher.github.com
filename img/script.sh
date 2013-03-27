34620dc02caa2716d2190ec836d900c673532205
Leander Herzog <info@leanderherzog.ch>
2013-03-27 15:21:40 +0100

    ...

--- img/script.sh
+++ img/script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-convert in/* -resize 1600x800^ +level-colors midnightblue,whitesmoke -channel blue -ordered-dither h4x4a -quality 80% -set filename:fname 'out/%t' +adjoin '%[filename:fname].jpg'
+convert in/* -resize 1600x800^ +level-colors midnightblue,whitesmoke -channel blue -quality 80% -set filename:fname 'out/%t' +adjoin '%[filename:fname].jpg'
